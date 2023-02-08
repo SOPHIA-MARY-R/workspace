f9474fd272e02ced9190ddad1ccdfa064342f98f
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-02-08 20:15:03 +0530

    Configured models.py

--- frontend/backend/backend/urls.py
+++ frontend/backend/backend/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('images/', include('images.urls'))
+    path('admin/', admin.site.urls)
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- frontend/backend/images/models.py
+++ frontend/backend/images/models.py
@@ -1,3 +1,29 @@
 from django.db import models
-
+import PIL
+import numpy as np
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
+from io import BytesIO
+import os
+from django.core.files.base import ContentFile
 # Create your models here.
+class Image(models.Model):
+    pic=models.ImageField(upload_to="images")
+    rmbg_pic=models.ImageField(upload_to="images_rmbg")
+
+    def __str__(self):
+        return str(self.id)
+
+    def save(self, *args, **kwargs):
+        pil_pic=PIL.Image.open(self.pic)
+        pic=np.array(pil_pic)
+        segmentor=SelfiSegmentation()
+        rmbg=segmentor.removeBG(pic, (0, 255, 0), threshold=0.4)
+        buffer=BytesIO()
+        output_pic=PIL.Image.fromarray(rmbg)
+        output_pic.save(buffer, format="png")
+        val=buffer.getvalue()
+        filename=os.path.basename(self, pic.name)
+        name, extension = filename.split(".")
+        self.rmbg_pic(f"bgrm_{name}.png", ContentFile(val), save=False)
+        super().save(*args, **kwargs)
+
