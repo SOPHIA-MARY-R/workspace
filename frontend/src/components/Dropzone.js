143828ff9fd822e9fde568c300376620dfa10aeb
SOPHIA MARY R <soph20410.cs@rmkec.ac.in>
2023-02-10 19:56:17 +0530

    Dropzone & Alert setup

---
+++ frontend/src/components/Dropzone.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Alert from "./Alert";
+
+const url = "http://127.0.0.1:8000/api";
+
+export default function DropZone(){
+    //state for displaying alert
+
+    //dropzone setup
+
+    //send image
+
+    // get image
+
+    return (
+        <>
+            <Alert/>
+            dropzone
+        </>
+    );
+}
